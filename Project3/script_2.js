a37a1dbc03629c21dc67ed63396f813a24d20717
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-08-12 22:12:44 -0700

    update v.02.09.16

--- Project3/script_2.js
+++ Project3/script_2.js
@@ -3,27 +3,32 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
 	constructor() {
 		this.image = new Image();
-		this.image.src = './enemies/enemy1.png';
-		// this.speed = Math.random() * 4 - 2;
-		this.spriteWidth = 293;
-		this.spriteHeight = 155;
-		this.width = this.spriteWidth / 2.5;
-		this.height = this.spriteHeight / 2.5;
+		this.image.src = './enemies/enemy2.png';
+		this.speed = Math.random() * 4 + 1;
+		this.spriteWidth = 266;
+		this.spriteHeight = 188;
+		this.width = this.spriteWidth / 2;
+		this.height = this.spriteHeight / 2;
 		this.x = Math.random() * (canvas.width - this.width);
 		this.y = Math.random() * (canvas.height - this.height);
 		this.frame = 0;
 		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+		this.angle = 0;
+		this.angleSpeed = Math.random() * 0.2;
+		this.curve = Math.random() * 7;
 	}
 	update() {
-		this.x += Math.random() * 5 - 2.5;
-		this.y += Math.random() * 5 - 2.5;
+		this.x -= this.speed;
+		this.y += this.curve * Math.sin(this.angle);
+		this.angle += this.angleSpeed;
+		if (this.x + this.width < 0) this.x = canvas.width;
 		// animate sprites
 		if (gameFrame % this.flapSpeed === 0) {
 			this.frame > 4 ? (this.frame = 0) : this.frame++;
--- Project3/script_3.js
+++ Project3/script_3.js
@@ -3,27 +3,36 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 50;
 const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
 	constructor() {
 		this.image = new Image();
-		this.image.src = './enemies/enemy1.png';
-		// this.speed = Math.random() * 4 - 2;
-		this.spriteWidth = 293;
-		this.spriteHeight = 155;
-		this.width = this.spriteWidth / 2.5;
-		this.height = this.spriteHeight / 2.5;
+		this.image.src = './enemies/enemy3.png';
+		this.speed = Math.random() * 4 + 1;
+		this.spriteWidth = 218;
+		this.spriteHeight = 177;
+		this.width = this.spriteWidth / 2;
+		this.height = this.spriteHeight / 2;
 		this.x = Math.random() * (canvas.width - this.width);
 		this.y = Math.random() * (canvas.height - this.height);
 		this.frame = 0;
 		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+		this.angle = 0;
+		this.angleSpeed = Math.random() * 0.5 + 0.5;
+		// this.curve = Math.random() * 200 + 50;
 	}
 	update() {
-		this.x += Math.random() * 5 - 2.5;
-		this.y += Math.random() * 5 - 2.5;
+		this.x =
+			(canvas.width / 2) * Math.sin((this.angle * Math.PI) / 90) +
+			(canvas.width / 2 - this.width / 2);
+		this.y =
+			(canvas.height / 2) * Math.cos((this.angle * Math.PI) / 360) +
+			(canvas.height / 2 - this.height / 2);
+		this.angle += this.angleSpeed;
+		if (this.x + this.width < 0) this.x = canvas.width;
 		// animate sprites
 		if (gameFrame % this.flapSpeed === 0) {
 			this.frame > 4 ? (this.frame = 0) : this.frame++;
