7d7409dfeaec45094ecc193225da682bb5017075
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-09-03 20:58:43 -0700

    :v.7.03.00

--- Project8/player.js
+++ Project8/player.js
@@ -1,18 +1,57 @@
+import {
+	StandingLeft,
+	StandingRight,
+	SittingLeft,
+	SittingRight,
+	RunningLeft,
+	RunningRight,
+	JumpingLeft,
+	JumpingRight,
+	FallingLeft,
+	FallingRight,
+} from './state.js';
+
 export default class Player {
 	constructor(gameWidth, gameHeight) {
 		this.gameWidth = gameWidth;
 		this.gameHeight = gameHeight;
-		this.states = [];
-		this.currentState = this.states[0];
+		this.states = [
+			new StandingLeft(this),
+			new StandingRight(this),
+			new SittingLeft(this),
+			new SittingRight(this),
+			new RunningLeft(this),
+			new RunningRight(this),
+			new JumpingLeft(this),
+			new JumpingRight(this),
+			new FallingLeft(this),
+			new FallingRight(this),
+		];
+		this.currentState = this.states[1];
 		this.image = document.getElementById('dogImage');
 		this.width = 200;
 		this.height = 181.83;
 		this.x = this.gameWidth / 2 - this.width / 2;
 		this.y = this.gameHeight - this.height;
+		this.vy = 0;
+		this.weight = 1;
 		this.frameX = 0;
 		this.frameY = 0;
+		this.maxFrame = 6;
+		this.speed = 0;
+		this.maxSpeed = 12;
+		this.fps = 200;
+		this.frameTimer = 0;
+		this.frameInterval = 1000 / this.fps;
 	}
-	draw(context) {
+	draw(context, deltaTime) {
+		if (this.frameTimer > this.frameInterval) {
+			if (this.frameX < this.maxFrame) this.frameX++;
+			else this.frameX = 0;
+			this.frameTimer = 0;
+		} else {
+			this.frameTimer += deltaTime;
+		}
 		context.drawImage(
 			this.image,
 			this.width * this.frameX,
@@ -24,5 +63,30 @@ export default class Player {
 			this.width,
 			this.height
 		);
+	}
+	update(input) {
+		this.currentState.handleInput(input);
+		// horizontal movement
+		this.x += this.speed;
+		if (this.x <= 0) this.x = 0;
+		else if (this.x >= this.gameWidth - this.width)
+			this.x = this.gameWidth - this.width;
+		// vertical movement
+		this.y += this.vy;
+		if (!this.onGround()) {
+			this.vy += this.weight;
+		} else {
+			this.vy = 0;
+		}
+		// Double check player doesn't fall through floor;
+		if (this.y > this.gameHeight - this.height)
+			this.y = this.gameHeight - this.height;
+	}
+	setState(state) {
+		this.currentState = this.states[state];
+		this.currentState.enter();
+	}
+	onGround() {
+		return this.y >= this.gameHeight - this.height;
 	}
 }
--- Project8/script.js
+++ Project8/script.js
@@ -13,12 +13,15 @@ window.addEventListener('load', function () {
 
 	const player = new Player(canvas.width, canvas.height);
 	const input = new InputHandler();
-
-	function animate() {
+	let lastTime = 0;
+	function animate(timeStamp) {
+		const deltaTime = timeStamp - lastTime;
+		lastTime = timeStamp;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		player.draw(ctx);
-		drawStatusText(ctx, input);
+		player.update(input.lastKey);
+		player.draw(ctx, deltaTime);
+		drawStatusText(ctx, input, player);
 		requestAnimationFrame(animate);
 	}
-	animate();
+	animate(0);
 });
